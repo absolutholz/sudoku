84b3297ab357fe15bbd81c358c8dfe45acdb2bf7
absolutholz <absolutholz@gmail.com>
2020-01-04 23:46:31 +0100

    Update timer story

--- src/components/Timer/Timer.stories.js
+++ src/components/Timer/Timer.stories.js
@@ -16,3 +16,33 @@ export const basic = () => ({
 	},
 	template: '<timer :seconds="seconds" />',
 });
+
+export const seconds = () => ({
+	components: { Timer },
+	props: {
+		seconds: {
+			default: number('Seconds', 59, { min: 0, step: 1 }),
+		},
+	},
+	template: '<timer :seconds="seconds" />',
+});
+
+export const minutes = () => ({
+	components: { Timer },
+	props: {
+		seconds: {
+			default: number('Seconds', 2500, { min: 0, step: 1 }),
+		},
+	},
+	template: '<timer :seconds="seconds" />',
+});
+
+export const hours = () => ({
+	components: { Timer },
+	props: {
+		seconds: {
+			default: number('Seconds', 59000, { min: 0, step: 1 }),
+		},
+	},
+	template: '<timer :seconds="seconds" />',
+});
