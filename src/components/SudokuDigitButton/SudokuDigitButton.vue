fa3b536abe5284272e483cd99a699fba0e2a6a28
absolutholz <absolutholz@gmail.com>
2020-01-06 17:21:37 +0100

    Refactor sudoku digit button

---
+++ src/components/SudokuDigitButton/SudokuDigitButton.vue
@@ -0,0 +1,25 @@
+<template>
+	<btn
+		class="btn--digit"
+	><slot /></btn>
+</template>
+
+<script>
+import Btn from './../Btn';
+
+export default {
+	name: 'SudokuDigitButton',
+
+	components: {
+		Btn,
+	},
+};
+</script>
+
+<style lang="scss">
+.btn--digit {
+	font: 2rem / 1 "Rubik", sans-serif;
+	min-height: 1.5em;
+	min-width: 1.5em;
+}
+</style>
