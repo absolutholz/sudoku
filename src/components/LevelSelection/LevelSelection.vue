3318bd9abd46b06a55e8259afc621b18f5bf4e22
absolutholz <absolutholz@gmail.com>
2020-03-15 21:36:08 +0100

    Add level selection component

---
+++ src/components/LevelSelection/LevelSelection.vue
@@ -0,0 +1,44 @@
+<template>
+	<ol class="levels-list">
+		<li v-for="(display, level) in levels" :key="`difficulty-level-${ level }`">
+			<btn size="full" variant="outlined" @click="onLevelSelection(level)">{{ display }}</btn>
+		</li>
+	</ol>
+</template>
+
+<script>
+import Btn from './../../components/Btn';
+
+export default {
+	name: 'LevelSelection',
+
+	components: {
+		Btn,
+	},
+
+	props: {
+		levels: {
+			required: true,
+			type: Object,
+		},
+	},
+
+	methods: {
+		onLevelSelection (level) {
+			this.$emit('levelSelection', { level });
+		},
+	},
+}
+</script>
+
+<style lang="scss">
+.levels-list {
+	all: unset;
+	display: inline-grid;
+	grid-gap: var(--gutter);
+	grid-template-columns: repeat(2, 1fr);
+	list-style: none;
+	max-width: 600px;
+	width: 100%;
+}
+</style>
