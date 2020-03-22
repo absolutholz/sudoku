f1b72bef3d321f4d7c9f712e667a2daa06181e58
absolutholz <absolutholz@gmail.com>
2020-03-22 17:29:16 +0100

    Add cell number svg

--- src/components/CellNumber/CellNumber.stories.js
+++ src/components/CellNumber/CellNumber.stories.js
@@ -1,53 +1,20 @@
 import { withKnobs, number, boolean } from '@storybook/addon-knobs';
 
-import CellNumber from './CellNumber.vue';
+import CellNumber from '.';
 
 export default {
-	title: 'Molecules',
+	title: 'Atoms',
 	decorators: [withKnobs],
 };
 
 export const cellNumber = () => ({
 	components: { CellNumber },
 	template: `
-		<div style="line-height: 1; xwidth: 200px;">
-			<cell-number
-				:digit="digit"
-			></cell-number>
-		</div>
+			<cell-number>{{ digit }}</cell-number>
 	`,
 	props: {
 		digit: {
 			default: number('Digit', 1, { max: 9, min: 1, step: 1 }),
 		},
-	// 	isOriginal: {
-	// 		default: boolean('Original', false),
-	// 	},
-	// 	isActive: {
-	// 		default: boolean('Active', false),
-	// 	},
-	// 	isInvalid: {
-	// 		default: boolean('Invalid', false),
-	// 	},
-	// 	isPeerCell: {
-	// 		default: boolean('Peer Cell', false),
-	// 	},
-	// 	isPeerDigit: {
-	// 		default: boolean('Peer Digit', false),
-	// 	},
 	},
-	// template: `
-	// 	<sudoku-cell
-	// 		:col="0"
-	// 		:row="0"
-	// 		:subgrid="0"
-	// 		:isOriginal="isOriginal"
-	// 		:isActive="isActive"
-	// 		:isInvalid="isInvalid"
-	// 		:isPeerCell="isPeerCell"
-	// 		:isPeerDigit="isPeerDigit"
-	// 	>
-	// 		{{ digit }}
-	// 	</sudoku-cell>
-	// `,
 });
